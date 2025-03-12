21f76a08566ac9a27ceb7d3056d88387ae32aa3e
Gerald Sebastian <gerrysebs2018@gmail.com>
2025-03-12 12:19:16 -0500

    Initial commit: Quantitative Momentum Strategy

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,9 @@
 """
-Quantitative Momentum Trading Strategy Package
+Quantitative Momentum Strategy Package
+
+This package implements a momentum-based trading strategy for S&P 500 stocks,
+combining traditional technical analysis with machine learning enhancements.
 """
 
-__version__ = '0.1.0' 
+__version__ = '1.0.0'
+__author__ = 'Gerald' 
