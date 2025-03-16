a07d2557e07713e3a89415561b88bb75fb8a7de2
Gerald Sebastian <gerrysebs2018@gmail.com>
2025-03-16 16:19:27 -0500

    Fix module imports in src/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ Quant Momentum Trading Algorithm Package
 """
 
 from .strategy import run_strategy, get_cached_signals, RELIABLE_TICKERS
-from .cache import clear_cache
+from .cache import redis_client, clear_cache
 
 __version__ = '1.0.0'
 
